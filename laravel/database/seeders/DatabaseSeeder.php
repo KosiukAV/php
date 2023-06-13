584b8f0c5139eff55b7df09cd94fb95bbea2b9ca
Александр Косюк <KosiukAV@mosreg.ru>
2023-06-13 23:12:17 +0300

    init database

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $articles = Article::factory()->count(100)->create();
+        $tags = Tag::factory()->count(100)->create();
+
+        foreach($articles as $item)
+        {
+            $item->tags()->attach($tags->random(rand(1, 5)));
+        }
     }
 }
